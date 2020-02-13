59bfa711e9a86b5cc735ac24a18dfaddda6c9c3f
Zach Sexton <zsexton2011@gmail.com>
2020-02-13 02:14:45 -0500

    Add openweathermap api call

--- weather.js
+++ weather.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const port = 3000;
+
+let apiKey = process.env.WEATHER_API_KEY;
+console.log(apiKey);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -11,14 +16,32 @@ app.get('/weather', (req, res) => {
     res.send(weatherData);
 });
 
+app.get('/current-weather', (req, res) => {
+    if (Object.keys(req.query).length === 0) {
+        res.status(400).send('Please provide a city.');
+        res.send()
+    }
+
+    let city = req.query.city;
+
+    let weatherResponse = getCurrentWeather(city);
+
+    res.send(weatherResponse);
+});
+
 app.listen(port, () => console.log(`Weather api listening on port ${port}!`));
 
-function getWeatherForecast(startDate) {
-    return {
-        "forecast:": [
-            { "date": startDate, "high": 78, "low": 56 },
-            { "date": startDate, "high": 78, "low": 56 },
-            { "date": startDate, "high": 78, "low": 56 },
-        ]
-    };
+function getCurrentWeather(city) {
+    // axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q=fishers&appid=' + apiKey)
+        .then(response => {
+            console.log(response);
+            console.log(response.data);
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+            return error;
+        });
 }
+
